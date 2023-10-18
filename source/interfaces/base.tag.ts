e42f3867a6a9961cc75f15ffc1043f81a2ff4fc6
Salem <salem.djekoun@gmail.com>
2023-10-18 21:09:11 +0200

    Create base tag interface

--- source/interfaces/base.tag.ts
+++ source/interfaces/base.tag.ts
@@ -1,9 +1,11 @@
 
+import { GlobalAttributes } from "./global.attributes";
+
 /**
     * Represents a base tag element.
 */
 
-export interface BaseTag {
+export interface BaseTag extends GlobalAttributes {
 
     /** 
         * Gets or sets the baseline URL on which relative links are based.  
@@ -15,6 +17,8 @@ export interface BaseTag {
         * Sets or retrieves the window or frame at which to target content.
     */
 
-    target?: string;
-
+    target?: "_self"
+    | "_blank"
+    | "_parent"
+    | "_top";
 }
