2965b5d98ee4fda5b8f235dd6459760c3e2a89f9
Salem <salem.djekoun@gmail.com>
2023-10-18 16:51:27 +0200

    Create link tag interface

--- source/interfaces/link.tag.ts
+++ source/interfaces/link.tag.ts
@@ -1,6 +1,6 @@
 
 /**
-    * Represents a link tag element.
+    * Represents properties commonly used for HTML link tags.
 */
 
 export interface LinkTag {
@@ -12,8 +12,58 @@ export interface LinkTag {
     href: string;
 
     /**
-        * The relationship between the current document and the linked resource.
+        * The MIME type of the linked resource.
+    */
+
+    type?: string;
+
+    /**
+        * Specifies the relationship between the current document and the linked resource.
     */
 
     rel: string;
+
+    /**
+        * The type of the linked resource, such as "audio", "document", "embed", etc.
+    */
+
+    as?: "audio" 
+    | "document"
+    | "embed"
+    | "fetch"
+    | "font"
+    | "image"
+    | "object"
+    | "style"
+    | "track"
+    | "video"
+    | "worker";
+
+    /**
+        * How the element handles crossorigin requests.
+    */
+
+    crossOrigin?: "anonymous" | "use-credentials";
+
+    /**
+        * The language of the linked resource.
+    */
+
+    hreflang?: string;
+
+    /**
+        * Specifies the referrer policy for the linked resource.
+    */
+
+    referrerPolicy?: "no-referrer" 
+    | "no-referrer-when-downgrade" 
+    | "origin" 
+    | "origin-when-cross-origin" 
+    | "unsafe-url";
+
+    /**
+        * The cryptographic hash value of the linked resource.
+    */
+
+    integrity?: string;
 }
