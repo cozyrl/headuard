2f5c387998180dc33cf1db7aab557dc632cd485b
Salem <salem.djekoun@gmail.com>
2023-10-18 20:42:24 +0200

    Create script tag interface

--- source/interfaces/script.tag.ts
+++ source/interfaces/script.tag.ts
@@ -1,13 +1,70 @@
 
+import { GlobalAttributes } from "./global.attributes";
+
 /**
-    * Represents a script tag element with specified CSS properties.
+    * Represents properties commonly used for HTML script tags.
 */
 
-export interface ScriptTag {
+export interface ScriptTag extends GlobalAttributes {
 
     /**
         * The URL of the external script file.
     */
-   
+
     src: string;
+
+    /**
+        * MIME type for the associated scripting engine.
+    */
+
+    type?: "text/javascript" | "module" | "importmap";
+
+    /**
+        * Indicates that the script should be executed asynchronously.
+    */
+
+    async?: boolean;
+
+    /**
+        * The CORS setting for the script.
+    */
+
+    crossOrigin?: string | null;
+
+    /**
+        * Indicates that the script should be executed after the page has finished parsing.
+    */
+
+    defer?: boolean; 
+
+    /**
+        * The cryptographic hash value of the linked resource.
+    */
+
+    integrity?: string;
+
+    /**
+        * Indicates that the script should be ignored if the browser does not support modules.
+    */
+
+    noModule?: boolean;
+
+    /**
+        * A cryptographic nonce that can be used in combination with CSP settings.
+    */
+
+    nonce?: string;
+
+    /**
+        * The referrer policy for the script.
+    */
+
+    referrerPolicy?: "no-referrer" 
+    | "no-referrer-when-downgrade" 
+    | "origin" 
+    | "origin-when-cross-origin"
+    | "same-origin"
+    | "strict-origin"
+    | "strict-origin-when-cross-origin" 
+    | "unsafe-url";
 }
