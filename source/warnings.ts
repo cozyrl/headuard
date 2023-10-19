617b1023dc74f8b4ea12df063f565374fb9d673b
Salem <salem.djekoun@gmail.com>
2023-10-19 12:10:00 +0200

    Referencing warning messages in a file.

---
+++ source/warnings.ts
@@ -0,0 +1,3 @@
+
+export const w001 = "This attribute must not be used if the src attribute is absent (i.e. for inline scripts), in this case it would have no effect.";
+export const w002 = "The defer attribute has no effect on module scripts — they defer by default.";
