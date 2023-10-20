d0190f85321a6ba2b1626ddabd5728c4827470ac
Salem <salem.djekoun@gmail.com>
2023-10-20 09:36:52 +0200

    Add warn messages.

--- source/utilities/warnings.ts
+++ source/utilities/warnings.ts
@@ -1,3 +1,6 @@
 
 export const w001 = "This attribute must not be used if the src attribute is absent (i.e. for inline scripts), in this case it would have no effect.";
 export const w002 = "The defer attribute has no effect on module scripts — they defer by default.";
+export const w003 = "The hreflang attribute can only be used if the href attribute is present.";
+export const w004 = "The imagesizes attribute can only be used for rel='preload' and as='image'.";
+export const w005 = "The imagesrcset attribute can only be used for rel='preload' and as='image'.";
